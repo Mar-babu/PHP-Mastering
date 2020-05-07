9b3f671b91c62e23333a85a875223415477f39ee
Mar-babu <ar_cse@yahoo.com>
2020-05-07 07:42:59 +0600

    error log not showing error And control error

---
+++ PHP17Error/e1.php
@@ -0,0 +1,13 @@
+<?php
+//error_reporting(0);
+//error_reporting(E_ALL);
+//error_reporting(E_WARNING);
+//error_reporting(E_WARNING | E_NOTICE);   //ei error gula amra php ini file thekeo run korte pari by php --ini or get_cfg_var
+//
+ini_set("display_errors",0);
+ini_set("display_startup_errors",0);
+echo 121/0;
+echo $book;
+
+
+//echo get_cfg_var("cfg_file_path"); //ei error gula amra php ini file thekeo run korte pari by php --ini or get_cfg_var
